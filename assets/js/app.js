92ab9d2bd33b336ab3902797843d364df5665b4c
kemtch19 <kemtch19@gmail.com>
2024-03-25 09:26:19 -0500

    Se agrega carpeta de assets

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+console.log("proyecto de javascript");
+console.log("Esto es una prueba desde la terminal");
+console.log("hola puerca");
+
+for (let index = 0; index < 6; index++) {
+  console.log('-');
+}
+
+console.log("Esto es una prueba para la nueva rama");
